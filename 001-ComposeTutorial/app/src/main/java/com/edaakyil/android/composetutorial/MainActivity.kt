b7f4f4050532aa32dc53c5dbdd6d32b63189a778
edaakyil <edaakyil@gmail.com>
2025-01-03 13:38:31 +0300

    Configure layout

--- 001-ComposeTutorial/app/src/main/java/com/edaakyil/android/composetutorial/MainActivity.kt
+++ 001-ComposeTutorial/app/src/main/java/com/edaakyil/android/composetutorial/MainActivity.kt
@@ -7,10 +7,19 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,13 +35,18 @@ data class Message(val author: String, val text: String)
 
 @Composable
 fun MessageCard(msg: Message) {
-    Row {
+    Row(modifier = Modifier.padding(all = 8.dp)) {
         Image(
             painter = painterResource(R.drawable.profile_picture),
-            contentDescription = "Contact profile picture"
+            contentDescription = "Contact profile picture",
+            modifier = Modifier
+                .size(40.dp)
+                .clip(CircleShape)
         )
+        Spacer(modifier = Modifier.width(8.dp))
         Column {
             Text(text = msg.author)
+            Spacer(modifier = Modifier.height(4.dp))
             Text(text = msg.text)
         }
     }
